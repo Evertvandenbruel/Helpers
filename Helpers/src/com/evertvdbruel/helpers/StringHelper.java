84178aae0cfaca14d93e01e2fe37d250eedfc2bc
Evert Van den Bruel <evert@10to1.be>
2013-04-23 20:24:39 +0200

    added format double method

--- Helpers/src/com/evertvdbruel/helpers/StringHelper.java
+++ Helpers/src/com/evertvdbruel/helpers/StringHelper.java
@@ -1,9 +1,17 @@
 package com.evertvdbruel.helpers;
 
+import java.text.DecimalFormat;
+
 public class StringHelper {
 
 	public static Boolean isEmpty(String string) {
 		return (string == null || string.equals(""));
+	}
+
+	// http://developer.android.com/reference/java/text/DecimalFormat.html
+	public static String formatNumber(Double number, String format) {
+		DecimalFormat decimalFormat = new DecimalFormat(format);
+		return decimalFormat.format(number);
 	}
 
 }
